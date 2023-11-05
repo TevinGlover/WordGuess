9ccdab4da2411c5a788bd15e24fb5a9da7435dc9
Tevin Glover <tevwork.tg@gmail.com>
2023-11-05 15:13:55 -0500

    looking at readyme

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -8,8 +8,30 @@ package com.github.zipcodewilmington;
  */
 public class Hangman {
 
+    // choose a random word from a list
 
 
+//while (you want to play) { //outer loop
+    // start the game
+    // set word guessed to false
+    // while (the word isn't guessed) { //inner loop
+    //  print the current game state
+//ask for a guess (a single letter)
+
+    // check the letter against the word
+    //using the two character arrays discussed above
+    // increment the number of guesses
+
+    // if the word is guessed
+    //player won, congrats
+
+    // if too many guesses
+    //player lost, too bad, quit game
+
+    // ask if player wants to play again
+
+    // display game over
+}
 
 
 
