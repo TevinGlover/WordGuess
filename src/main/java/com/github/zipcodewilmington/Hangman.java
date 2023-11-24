f8bdd54248e1b9929d592eaced377fd0d68407bc
Tevin Glover <tevwork.tg@gmail.com>
2023-11-23 21:46:11 -0500

    work

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,6 +1,8 @@
 package com.github.zipcodewilmington;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -9,39 +11,59 @@ import java.util.Scanner;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
- public static void main ( String [] args ) {
-
-     //Scanner playerCharGuess = new Scanner(); // player inputs
-
-     String[] animalsWithThreeLetters = "cat dog rat hen fox".split(" "); //array from 0 - 4
-     int random = (int) (Math.random() * animalsWithThreeLetters.length); // choose a random number from array
-
-     int arrayNum = random;
-     // the number selected form the random generator
-
-     String animalPicked = animalsWithThreeLetters[arrayNum]; // array number to String
-
-     //System.out.println(animalPicked); // this check the random name is working
-
-     char[] animalChars = animalPicked.toCharArray(); //the ANIMAL split into chars
 
 
+    public static void main(String[] args) {
+        Scanner playerAnwser = new Scanner(System.in);// start the game
+        //Scanner playerCharGuess = new Scanner(); // player inputs
+      String[]  animalsWithThreeLetters = "cat dog rat hen fox".split(" ");
+
+
+        System.out.println("* Welcome To Animal Word Guess *" + " \n" +"  ****************************");
+        System.out.println("  Lets play A GAME !!");
+        System.out.println("  TYPE BELOW: (Yes or No)");
+        playerAnwser.nextLine();
+        List<Character> playerGuess = new ArrayList<>();
+
+
+        if (playerAnwser.equals("YES".compareToIgnoreCase("yes"))) {
+            System.out.println(" Game Starting Now"); // StartGame();
+        } else  {
+            System.out.println(" thank you for your answer Bye!");
+            System.out.println(" Game Over !!");
+        }
+    }
+    //*****************************************************************
+    public static String RandRandomPick(String[] Array) {
+         // for any string array
+        int random = (int) (Math.random() * Array.length); // choose a random number from array
+        int arrayNum = random; // the number selected form the random generator
+
+
+        return Array[arrayNum];
+    }
+
+    public static void  printWord(String word, List<Character> playerGuess) {
+
+
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuess.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            } else {
+                System.out.print("_");
+
+            }
+
+        }
+    }
+
+//     System.out.println(animalPicked); // this check the random name is working
+//
+//     char[] animalChars = animalPicked.toCharArray(); //the ANIMAL split into chars
+
+//***********************************************************************************************
      //while (you want to play) { //outer loop
 
-     System.out.println("* welcome to animal word guess *");
-     Scanner playerAnwser = new Scanner(System.in);// start the game
-     System.out.println("Lets play A GAME !!");
-     playerAnwser.nextLine();
-     if (playerAnwser.equals("YES".compareToIgnoreCase("yes"))) {
-         for (int i = 0; i <= animalChars.length; i++) {
-
-
-         }
-
-
-     } else {
-         System.out.println(" thank you for your answer Bye!");
-     }
 
  }
 
@@ -67,7 +89,7 @@ public class Hangman {
 
      // display game over
 
- }
+
 
 
 
--- src/test/java/com/github/zipcodewilmington/HangmanTest.java
+++ src/test/java/com/github/zipcodewilmington/HangmanTest.java
@@ -1,6 +1,13 @@
 package com.github.zipcodewilmington;
-import org.junit.Test;
 import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.github.zipcodewilmington.Hangman.RandRandomPick;
+import static com.github.zipcodewilmington.Hangman.printWord;
+
 /**
  * @author xt0fer
  * @version 1.0.0
@@ -8,14 +15,119 @@ import org.junit.Assert;
  */
 public class HangmanTest {
 
-@Test
-    public void testrandomAnimalName(){
+    @Test
+    public void testrandomAnimalName1() {
+        String[] randomWord = {"fox", "bat", "cat" };
+        String word = RandRandomPick(randomWord);
+        String actual = word;
+        String expected = "bat";
+
+        Assert.assertSame(expected, actual);
+
+    }
 
 
+    @Test
+    public void testrandomAnimalName2() { // check if it can return on of the words
 
+        String[] randomWord = {"fox", "bat", "cat" };
+        String word = RandRandomPick(randomWord);
+        String pickOne = "fox ";
+        String pickTwo = "bat";
+        String pickThree = "cat";
+        String actual = word;
+        boolean isItRandom = Boolean.parseBoolean(String.valueOf(actual.equals(pickOne)
+                || actual.equals(pickTwo) || actual.equals(pickThree)));
+        Assert.assertTrue(isItRandom); // this was very annoying to do ( i will have to remember this pain)
+    }
+
+    @Test
+    public void TestCompareChar() {
+        List<Character> playerGuess = new ArrayList<>();
+        playerGuess.add('c');
+        playerGuess.add('a');
+        playerGuess.add('t');
+        String hidenWord = "cat";
+       char[] word = hidenWord.toCharArray();
+       ArrayList expected = new ArrayList<>();
+       expected.add(hidenWord);
+
+         printWord(hidenWord,playerGuess);
+        Assert.assertSame(expected,playerGuess);
+
+    }
 }
-
-
-   // @Test
-
-}
+//    @Test
+//    public void testIfGuessAreCorrect() {
+//        Character guess = 'f';
+//        Character expected = 'f';
+//        Boolean isCorrect = GuessAreCorrect(guess,expected);
+//        Assert.assertTrue(isCorrect);
+//    }
+//
+//    @Test
+//    public void testIfGuessAreWrong() {
+//        char guess = 't';
+//       char expected = 'f';
+//        Boolean isWrong = GuessAreWrong(guess,expected);
+//        Assert.assertTrue(isWrong);
+//    }
+//
+//    @Test
+//    public void testIfAllCorrect() {
+//        char[] guess = "bat".toCharArray();
+//        String word = "bat";
+//        IfAllCorrect(word, guess);
+//        boolean isWinner;
+//        Assert.assertTrue(isWinner);
+//    }
+//
+//
+//
+//    @Test
+//    public void testTurnCount(){
+//    char fristGuess = 'c';
+//    char secondGuess = 'a';
+//   int expected = TurnCount(fristGuess,secondGuess );
+//     int actual = 2;
+//     Assert.assertArrayEquals(expected , actual);
+//    }
+//
+//    @Test
+//    public void testCountUnderLimit(){
+//        int guessCount = 3;
+//
+//        int maxNuberOfTurns = 4;
+//        int actual = TurnCountUnderLimit(guessCount );
+//        boolean expected =  actual < maxNuberOfTurns;
+//
+//
+//        Assert.assertTrue(expected);
+//    }
+//
+//    @Test
+//    public void testCountOverLimit(){
+//        int guessCount = 5;
+//
+//        int maxNuberOfTurns = 4;
+//        int actual = TurnCountOverLimit(guessCount );
+//        boolean expected =  actual < maxNuberOfTurns;
+//
+//
+//        Assert.assertTrue(expected);
+//    }
+//
+//    @Test
+//    public void TestWin(){
+//       int count =0;
+//      Boolean isItAWin  = Win(CountUnderLimit(), GuessAreCorrect());
+//       Assert.assertTrue(isItAWin);
+//    }
+//
+//    @Test
+//    public void TestLost(){
+//        int count =0;
+//        Boolean isItsALoss  = Lost(turnCountOverLimit());
+//        Assert.assertTrue(isItsALoss);
+//    }
+//}
